d8ff833d931e1689cf6abdd8c0a65d7fb8e6e069
ahmetberktas <ahmetberktas50@gmail.com>
2024-09-08 14:13:03 +0300

    chat & details components

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
   return (
     <div className="chat">
       <div className="top">
@@ -18,16 +32,119 @@ const Chat = () => {
           <img src="./info.png" alt="info"></img>
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message up">
+          <img src="./avatar.png" alt=""></img>
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+              ratione eligendi inventore sequi, quam vero adipisci vitae quaerat
+              unde quidem, molestiae tempore aspernatur? Laborum veniam ea
+              possimus velit, itaque voluptatem?
+            </p>
+            <span>1 dakika önce</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="./cappadocia.jpg"></img>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+              ratione eligendi inventore sequi, quam vero adipisci vitae quaerat
+              unde quidem, molestiae tempore aspernatur? Laborum veniam ea
+              possimus velit, itaque voluptatem?
+            </p>
+            <span>1 dakika önce</span>
+          </div>
+        </div>
+        <div className="message up">
+          <img src="./avatar.png" alt=""></img>
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+              ratione eligendi inventore sequi, quam vero adipisci vitae quaerat
+              unde quidem, molestiae tempore aspernatur? Laborum veniam ea
+              possimus velit, itaque voluptatem?
+            </p>
+            <span>1 dakika önce</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+              ratione eligendi inventore sequi, quam vero adipisci vitae quaerat
+              unde quidem, molestiae tempore aspernatur? Laborum veniam ea
+              possimus velit, itaque voluptatem?
+            </p>
+            <span>1 dakika önce</span>
+          </div>
+        </div>
+        <div className="message up">
+          <img src="./avatar.png" alt=""></img>
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+              ratione eligendi inventore sequi, quam vero adipisci vitae quaerat
+              unde quidem, molestiae tempore aspernatur? Laborum veniam ea
+              possimus velit, itaque voluptatem?
+            </p>
+            <span>1 dakika önce</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+              ratione eligendi inventore sequi, quam vero adipisci vitae quaerat
+              unde quidem, molestiae tempore aspernatur? Laborum veniam ea
+              possimus velit, itaque voluptatem?
+            </p>
+            <span>1 dakika önce</span>
+          </div>
+        </div>
+        <div className="message up">
+          <img src="./avatar.png" alt=""></img>
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+              ratione eligendi inventore sequi, quam vero adipisci vitae quaerat
+              unde quidem, molestiae tempore aspernatur? Laborum veniam ea
+              possimus velit, itaque voluptatem?
+            </p>
+            <span>1 dakika önce</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+              ratione eligendi inventore sequi, quam vero adipisci vitae quaerat
+              unde quidem, molestiae tempore aspernatur? Laborum veniam ea
+              possimus velit, itaque voluptatem?
+            </p>
+            <span>1 dakika önce</span>
+          </div>
+        </div>
+        <div ref={endRef}></div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <img src="./photo.png"></img>
           <img src="./camera.png"></img>
           <img src="./microphone.png"></img>
         </div>
-        <input type="text" placeholder="Mesaj yazınız"></input>
+        <input
+          type="text"
+          placeholder="Mesaj yazınız"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></input>
         <div className="emoji">
-          <img src="./emoji.png"></img>
+          <img src="./emoji.png" onClick={() => setOpen((prev) => !prev)}></img>
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji}></EmojiPicker>
+          </div>
         </div>
         <button className="send-btn">Send</button>
       </div>
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,73 @@ import React from "react";
 import "./detail.css";
 
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt=""></img>
+        <h2>Oğuzcan Oğuz</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt=""></img>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt=""></img>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt=""></img>
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./cappadocia.jpg" alt=""></img>
+                <span>cappadocia.jpg</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"></img>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./cappadocia.jpg" alt=""></img>
+                <span>cappadocia.jpg</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"></img>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./cappadocia.jpg" alt=""></img>
+                <span>cappadocia.jpg</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"></img>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="./cappadocia.jpg" alt=""></img>
+                <span>cappadocia.jpg</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"></img>
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt=""></img>
+          </div>
+        </div>
+        <button className="block">Block User</button>
+        <button className="logout">Logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
